6fafba4705aefe7bab8e53b23ae4aa8d6ba4cadd
Harrison Li <harrison@lob.com>
2019-07-08 16:59:00 -0700

    feat(server): add base server with health checks

---
+++ pkg/server/server.go
@@ -0,0 +1,37 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/harrisonlob/goyagi/pkg/health"
+	"github.com/labstack/echo"
+	"github.com/lob/logger-go"
+	"github.com/harrisonlob/goyagi/pkg/signals"
+)
+
+// New returns a new HTTP server with the registered routes.
+func New() *http.Server {
+	log := logger.New()
+
+	e := echo.New()
+
+	health.RegisterRoutes(e)
+	srv:= &http.Server {
+		Addr: fmt.Sprintf(":%d", 3000),
+		Handler: e,
+	}
+
+	graceful := signals.Setup()
+
+	go func() {
+		<- graceful
+		err := srv.Shutdown(context.Background())
+		if err != nil {
+			log.Err(err).Error("server shutdown")
+		}
+	} ()
+
+	return srv
+}
