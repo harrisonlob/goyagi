2b34355fbdc29ea8d52f843eebb68f394259309d
Harrison Li <harrison@lob.com>
2019-07-10 11:31:44 -0700

    fixing spacing issues

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -6,9 +6,9 @@ import (
 	"net/http"
 
 	"github.com/harrisonlob/goyagi/pkg/health"
+	"github.com/harrisonlob/goyagi/pkg/signals"
 	"github.com/labstack/echo"
 	"github.com/lob/logger-go"
-	"github.com/harrisonlob/goyagi/pkg/signals"
 )
 
 // New returns a new HTTP server with the registered routes.
@@ -18,20 +18,21 @@ func New() *http.Server {
 	e := echo.New()
 
 	health.RegisterRoutes(e)
-	srv:= &http.Server{
-		Addr: fmt.Sprintf(":%d", 3000),
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", 3000),
 		Handler: e,
 	}
 
 	graceful := signals.Setup()
 
 	go func() {
-		<- graceful
+		<-graceful
 		err := srv.Shutdown(context.Background())
 		if err != nil {
 			log.Err(err).Error("server shutdown")
 		}
-	} ()
+	}()
 
 	return srv
 }
